db0c2f3d153d0c8e8d121870468c9d0fe38e0ee6
Jennifer Miller <jmiller6128@gmail.com>
2014-04-07 20:45:01 -0400

    General layout and header complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
 console.log('\'Allo \'Allo!');
+
+$(document).ready(function(){
+
+	$('#show-nav').on('click', function(){
+		if($('.nav-display').css('visibility') == 'hidden') {
+			$('.nav-display').css('visibility', 'visible');
+		} else {
+			$('.nav-display').css('visibility', 'hidden');
+		}
+	});
+
+	$('#close-nav').on('click', function(){
+		$('.nav-display').css('visibility', 'hidden');
+	});
+
+});
