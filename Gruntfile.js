716186c543a537ba98923dacf27c2880b3588f8d
Jennifer Miller <jmiller6128@gmail.com>
2014-04-11 09:07:00 -0400

    fixed a couple of typos

--- Gruntfile.js
+++ Gruntfile.js
@@ -147,7 +147,7 @@ module.exports = function (grunt) {
                 sassDir: '<%= yeoman.app %>/styles',
                 cssDir: '.tmp/styles',
                 generatedImagesDir: '.tmp/images/generated',
-                imagesDir: '<%= yeoman.app %>/images',
+                imagesDir: '<%= yeoman.app %>/    images',
                 javascriptsDir: '<%= yeoman.app %>/scripts',
                 fontsDir: '<%= yeoman.app %>/styles/fonts',
                 importPath: '<%= yeoman.app %>/bower_components',
